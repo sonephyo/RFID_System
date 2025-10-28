cd6004906cc0d0eca9c5ee8066fa0ddfd29b9b63
Soney <sonephyo7777777@gmail.com>
2025-10-28 18:32:59 -0400

    update: migration now includes reseting the whole table and filling with dummyData

--- rfid-backend/migrate/migrate.go
+++ rfid-backend/migrate/migrate.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 
 	"github.com/sonephyo/RFID_System/rfid-backend/initializers"
 	"github.com/sonephyo/RFID_System/rfid-backend/models"
+	"gorm.io/gorm"
 )
 
 func init() {
@@ -13,10 +16,18 @@ func init() {
 }
 
 func main() {
-	err := initializers.DB.AutoMigrate(&models.User{})
+
+	err := initializers.DB.Migrator().DropTable(&models.User{})
+	if err != nil {
+		log.Fatalf("failed to drop table: %v", err)
+	}
+	fmt.Printf("Dropping User table...\n")
+
+	err = initializers.DB.AutoMigrate(&models.User{})
 	if err != nil {
 		log.Fatal("Error: Migration Failed")
 	}
+	fmt.Printf("User table created...\n")
 
 	var dummyData = []models.User{
 		{
@@ -31,13 +42,27 @@ func main() {
 		},
 	}
 
-	for _, dummy := range dummyData {
-		result := initializers.DB.Create(&dummy)
+	for _, user := range dummyData {
+
+		var existingUser models.User
+		result := initializers.DB.Where(&models.User{Name: user.Name}).First(&existingUser)
 
 		if result.Error != nil {
-			log.Fatal("Error: error in adding dummy data into the db")
-			return
+			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+				result := initializers.DB.Create(&user)
+				if result.Error != nil {
+					log.Fatal("Error: error in adding user data into the db")
+					return
+				}
+				fmt.Printf("Added %s into User table\n", user.Name)
+				continue
+			} else {
+				fmt.Printf("Error: %v\n", err)
+				log.Fatal()
+			}
 		}
+
+		fmt.Printf("User already exists: %s\n", existingUser.Name)
 	}
 
 }
