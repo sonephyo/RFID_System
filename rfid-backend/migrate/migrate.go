7a89488b7c69afb52c3a05070036c67180b6f1f5
Soney <sonephyo7777777@gmail.com>
2025-10-28 18:09:57 -0400

    update: migration for setting up backend

--- rfid-backend/migrate/migrate.go
+++ rfid-backend/migrate/migrate.go
@@ -17,4 +17,27 @@ func main() {
 	if err != nil {
 		log.Fatal("Error: Migration Failed")
 	}
+
+	var dummyData = []models.User{
+		{
+			Name:   "Soney",
+			Age:    21,
+			CardID: "8061231234",
+		},
+		{
+			Name:   "Vandan",
+			Age:    22,
+			CardID: "8064324321",
+		},
+	}
+
+	for _, dummy := range dummyData {
+		result := initializers.DB.Create(&dummy)
+
+		if result.Error != nil {
+			log.Fatal("Error: error in adding dummy data into the db")
+			return
+		}
+	}
+
 }
