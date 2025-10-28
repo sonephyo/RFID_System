bc9c780b3bb262264fe504e76b7f738fa04ac101
Soney <sonephyo7777777@gmail.com>
2025-10-28 18:09:12 -0400

    update: get request to users in the database

--- rfid-backend/main.go
+++ rfid-backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/sonephyo/RFID_System/rfid-backend/controllers"
 	"github.com/sonephyo/RFID_System/rfid-backend/initializers"
 )
 
@@ -13,11 +14,11 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	user_r := r.Group("/users")
+	user_r.GET("/", controllers.GetUser)
+
+	// TODO: Implement attendence functionality after scanning
+	// attendence_r := r.Group("/attendences")
 
 	r.Run()
 }
